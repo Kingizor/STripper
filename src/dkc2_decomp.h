3776a1fee1cb9dfac65681e08d3c09b6e73b4f8b
Kingizor <kingizor@gmail.com>
2022-09-02 16:35:54 +0100

    remove scary old decompression code

--- src/dkc2_decomp.h
+++
@@ -1,8 +0,0 @@
-#ifndef dkc2_dec
-#define dkc2_dec
-
-void dkc2_decomp(unsigned char *rom, unsigned char *output, int *outlen, unsigned src);
-
-void dkc2_decode(unsigned char *rom, unsigned char *output, int *outlen, unsigned src);
-
-#endif
